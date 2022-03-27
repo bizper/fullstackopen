fdf28c868d83edd6a041a15bee3e8f3636a74732
bizper <needfast@hotmail.com>
2022-03-28 03:50:52 +0800

    createdata

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Persons = ({ person }) => {
   return (
@@ -19,32 +20,32 @@ const Filter = ({ search, handleSearch }) => {
 const OperatePanel = ({ handleSubmit, newName, newNumber, handleInput, handleNewNumber }) => {
   return (
     <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleInput} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <div>
+        name: <input value={newName} onChange={handleInput} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNewNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
   )
 }
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Yumi Matsutoya', number: '040-1234567', id: 0 },
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(resp => {
+      setPersons(resp.data)
+    })
+  }, [])
 
   const personsToShow = search.length > 0 ?
     persons.filter(item => item.name.toLowerCase().includes(search.toLowerCase())) : persons
